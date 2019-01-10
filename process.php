079be6b79347a3696f18e7097ddd53b1ae0801e8
Smellssea <46499691+Smellssea@users.noreply.github.com>
2019-01-10 19:18:41 +0100

    fill

--- process.php
+++ process.php
@@ -1,1 +1,16 @@
+<?php
+  if(isset($_POST['submit'])) {
+    
+    $to = "smellssea@gmail.com";
+    $subject = $_POST['subject'];
+    $email = $_POST['email'];
+    $txt = $_POST['message'];
+    $headers = "From: " .$email . "r/n" .
+      "CC: somebodyelse@example.com";
+    
+    mail($to, $subject, $email, $txt, $headers );
+    
+    header(. "Location: contact.html")
 
+  }
+?>
